fc734e5edf920ce416fba03de18f7ed4c58a5c86
beerent <brentryczak@gmail.com>
2020-11-26 14:28:02 -0600

    game works

--- 2020/11/26/rock_paper_scissors.go
+++ 2020/11/26/rock_paper_scissors.go
@@ -6,32 +6,45 @@ import (
     "os"
     "strconv"
     "strings"
+    "math/rand"
+    "time"
 )
 
-func getNumberOfRounds() (rounds int) {
-	roundsQuestion := "Best of how many rounds?"
-    rounds = getIntegerInput(roundsQuestion)
+func printHeader() {
+	fmt.Println("Welcome To Rock Paper Scissors Shoot (11/26/2020)")
+}
+
+func getNumberOfRounds() int {
+	roundsQuestion := "\nBest of how many rounds?"
+
+	var rounds int
     for {
+        rounds = getIntegerInput(roundsQuestion)
+
     	if rounds % 2 == 1 {
     		break
     	}
     	fmt.Println("must be an odd number of rounds.")
-
-    	rounds = getIntegerInput(roundsQuestion)
     }
 
-	return
+	return rounds
+}
+
+func getStringInput(message string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print(message, " ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSuffix(input, "\n")
+
+	return input
 }
 
 func getIntegerInput(message string) int {
-	reader := bufio.NewReader(os.Stdin)
-
-    var i int
+	var i int
 
     for {
-    	fmt.Print(message, " ")
-    	input, _ := reader.ReadString('\n')
-    	input = strings.TrimSuffix(input, "\n")
+    	input := getStringInput(message)
     	intVar, err := strconv.Atoi(input)
 
     	if (err == nil) {
@@ -45,10 +58,162 @@ func getIntegerInput(message string) int {
 	return i
 }
 
-func main() {
-    fmt.Println("Welcome To Rock Paper Scissors Shoot (11/26/2020)")
-    fmt.Println("")
-    rounds := getNumberOfRounds()
+func isValidUserDecision(userDecision string) bool {
+	return userDecision == "r" || userDecision == "p" || userDecision == "s"
+}
 
-    fmt.Println(rounds)
+func getUserDecision() string {
+	decisionQuestion := "\nRock (r), Paper (p), or Scissors (s)?"
+	
+	var userDecision string
+	for {
+		userDecision = strings.ToLower(getStringInput(decisionQuestion))
+		if ! isValidUserDecision(userDecision) {
+			fmt.Println("invalid option '" + userDecision + "'")
+			userDecision = getStringInput(decisionQuestion)
+		}
+
+		break
+	}
+
+	return userDecision
+}
+
+func printRoundsHeader(rounds int) {
+	if (rounds == 1) {
+		fmt.Println("\nBest of 1 round. Good luck!")
+	} else {
+		fmt.Println("\nBest of", rounds, "rounds. Good luck!")
+	}
+}
+
+func getComputerDecision() string {
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(3)
+
+	if randomNumber == 0 {
+		return "r"
+	} else if randomNumber == 1 {
+		return "p"
+	}
+
+	return "s"
+}
+
+func getRoundWinner(userDecision, computerDecision string) string {
+	switch userDecision {
+	case "r":
+		switch computerDecision {
+		case "s":
+			return "user"
+		case "p":
+			return "computer"
+		}
+		break
+
+	case "p":
+		switch computerDecision {
+		case "r":
+			return "user"
+		case "s":
+			return "computer"
+		}
+		break
+
+	case "s":
+		switch computerDecision {
+		case "p":
+			return "user"
+		case "r":
+			return "computer"
+		}
+		break
+	}
+
+	return "tie"
+}
+
+func getFullAttackName(shortname string) (fullname string) {
+	switch shortname {
+		case "r":
+			fullname = "rock"
+		case "p":
+			fullname =  "paper"
+		case "s":
+			fullname = "scissors"
+		}
+
+	return
+}
+
+func printRoundResults(userDecision, computerDecision, roundWinner string) {
+	fmt.Println("User throws\t", getFullAttackName(userDecision))
+	fmt.Println("Computer throws\t", getFullAttackName(computerDecision))
+	fmt.Println(roundWinner + "!")
+}
+
+func winnerIsUser(winner string) bool {
+	return winner == "user"
+}
+
+func winnerIsComputer(winner string) bool {
+	return winner == "computer"
+}
+
+func getWinner(userWins, computerWins, rounds int) (winner string, gameover bool) {
+	winsNeeded := (rounds + 1) / 2
+
+	if userWins >= winsNeeded {
+		return "user", true
+	}
+
+	if computerWins >= winsNeeded {
+		return "computer", true
+	}
+
+	return "", false
+}
+
+func printWinner (winner string) {
+	if winner == "user" {
+		fmt.Println("\nCongratulations! You are the winner.")
+	} else {
+		fmt.Println("\nYou lost! :(")
+	}
+}
+
+func runGame(rounds int) {
+	printRoundsHeader(rounds);
+
+	userWins := 0
+	computerWins := 0
+
+	var winner string
+	
+	for {
+		userDecision := getUserDecision()
+		computerDecision := getComputerDecision()
+		roundWinner := getRoundWinner(userDecision, computerDecision)
+		printRoundResults(userDecision, computerDecision, roundWinner)
+
+		if winnerIsUser(roundWinner) {
+			userWins++
+		} else if winnerIsComputer(roundWinner) {
+			computerWins++
+		}
+
+		winnerName, gameover := getWinner(userWins, computerWins, rounds)
+		if gameover {
+			winner = winnerName
+			break
+		}
+	}
+
+	printWinner(winner)
+}
+
+func main() {
+    printHeader()
+    rounds := getNumberOfRounds()
+    runGame(rounds)
 }
