1825231779009b0468a546726a6555ef293fe30b
beerent <brentryczak@gmail.com>
2020-11-28 15:26:43 -0600

    completed linked list dojo

--- 2020/11/28/linked_list_utils.go
+++ 2020/11/28/linked_list_utils.go
@@ -5,6 +5,13 @@ import (
 	"strconv"
 )
 
+type SortType int
+
+const (
+    ASC = iota
+    DESC
+)
+
 func add(node *Node, data int) {
 	var nextNode Node
 	nextNode.data = data
@@ -16,9 +23,94 @@ func add(node *Node, data int) {
 	node.next = &nextNode
 }
 
-//func reverse(list LinkedList) LinkedList {
-//	return list
-//}
+func set(node *Node, data int) {
+	var nextNode Node
+	nextNode.data = data
+
+	node.next = &nextNode
+}
+
+func remove(node *Node) {
+	node.next = nil
+}
+
+func swap(nodeA, nodeB *Node) {
+	var tempNode Node
+
+	tempNode.data = nodeA.data
+	nodeA.data = nodeB.data
+	nodeB.data = tempNode.data
+}
+
+func count(node *Node) int {
+	count := 0
+	countNode := node
+	for {
+		if countNode == nil {
+			break
+		}
+
+		count++
+		countNode = countNode.next
+	}
+
+	return count
+}
+
+func sort(node *Node, sortType SortType) {
+	switch sortType {
+	case ASC:
+	    sortAsc(node)
+	    break;
+	case DESC:
+	    sortDesc(node)
+	    break;
+	}
+}
+
+func sortAsc(node *Node) {
+	trackingNode := node
+	currentNode := node
+
+	for {
+		if currentNode == nil {
+			break
+		}
+
+		swapMade := false
+
+		for {
+			if currentNode.next == nil || currentNode.data <= currentNode.next.data {
+				break
+			}
+
+			swap(currentNode, currentNode.next)
+			swapMade = true
+			currentNode = currentNode.next
+		}
+
+		if !swapMade {
+			trackingNode = trackingNode.next
+		}
+		currentNode = trackingNode
+	}
+}
+
+func sortDesc(node *Node) {
+	sortAsc(node)
+	reverse(node)
+}
+
+func reverse(node *Node) {
+	iterations := count(node)
+	for i := iterations; i > 0; i-- {
+		currentNode := node
+		for j := 1; j < i; j++ {
+			swap(currentNode, currentNode.next)
+			currentNode = currentNode.next
+		}
+	}
+}
 
 func intToString(i int) string {
 	return strconv.Itoa(i)
--- 2020/11/28/linked_list_utils_test.go
+++ 2020/11/28/linked_list_utils_test.go
@@ -1,7 +1,6 @@
 package linked_list
 
 import (
-	//"fmt"
 	"testing"
 )
 
@@ -17,5 +16,162 @@ func TestAdd(t *testing.T) {
 
 	if listAsString != "1, 4, 3, 2" {
 		t.Errorf("listAsString() expected '1, 4, 3, 2', found '" + listAsString + "'")
+	}
+}
+
+func TestSet(t *testing.T) {
+	var node Node
+	node.data = 1
+
+	var nextNode Node
+	nextNode.data = 2
+	node.next = &nextNode
+
+	set(&node, 3)
+
+	listAsString := toString(node)
+
+	if listAsString != "1, 3" {
+		t.Errorf("listAsString() expected '1, 3', found '" + listAsString + "'")
+	}
+}
+
+func TestCount(t *testing.T) {
+	var node Node
+	node.data = 4
+
+	var node2 Node
+	node2.data = 2
+	node.next = &node2
+
+	var node3 Node
+	node3.data = 1
+	node2.next = &node3
+
+	var node4 Node
+	node4.data = 3
+	node3.next = &node4
+
+	count := count(&node)
+
+	if count != 4 {
+		t.Errorf("listAsString() expected '4', found '" + intToString(count) + "'")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	var node Node
+	node.data = 1
+
+	var nextNode Node
+	nextNode.data = 2
+	node.next = &nextNode
+
+	remove(&node)
+
+	listAsString := toString(node)
+
+	if listAsString != "1" {
+		t.Errorf("listAsString() expected '1', found '" + listAsString + "'")
+	}
+}
+
+func TestSwap (t *testing.T) {
+	var node Node
+	node.data = 4
+
+	var node2 Node
+	node2.data = 2
+	node.next = &node2
+
+	var node3 Node
+	node3.data = 1
+	node2.next = &node3
+
+	var node4 Node
+	node4.data = 3
+	node3.next = &node4
+
+	swap(&node2, &node4)
+
+	listAsString := toString(node)
+
+	if listAsString != "4, 3, 1, 2" {
+		t.Errorf("listAsString() expected '4, 3, 1, 2', found '" + listAsString + "'")
+	}
+}
+
+func TestReverse(t *testing.T) {
+	var node Node
+	node.data = 4
+
+	var node2 Node
+	node2.data = 2
+	node.next = &node2
+
+	var node3 Node
+	node3.data = 1
+	node2.next = &node3
+
+	var node4 Node
+	node4.data = 3
+	node3.next = &node4
+
+	reverse(&node)
+
+	listAsString := toString(node)
+
+	if listAsString != "3, 1, 2, 4" {
+		t.Errorf("TestReverse() expected '3, 1, 2, 4', found '" + listAsString + "'")
+	}
+}
+
+func TestSortAsc(t *testing.T) {
+	var node Node
+	node.data = 4
+
+	var node2 Node
+	node2.data = 2
+	node.next = &node2
+
+	var node3 Node
+	node3.data = 1
+	node2.next = &node3
+
+	var node4 Node
+	node4.data = 3
+	node3.next = &node4
+
+	sortAsc(&node)
+
+	listAsString := toString(node)
+
+	if listAsString != "1, 2, 3, 4" {
+		t.Errorf("listAsString() expected '1, 2, 3, 4', found '" + listAsString + "'")
+	}
+}
+
+func TestSortDesc(t *testing.T) {
+	var node Node
+	node.data = 4
+
+	var node2 Node
+	node2.data = 2
+	node.next = &node2
+
+	var node3 Node
+	node3.data = 1
+	node2.next = &node3
+
+	var node4 Node
+	node4.data = 3
+	node3.next = &node4
+
+	sortDesc(&node)
+
+	listAsString := toString(node)
+
+	if listAsString != "4, 3, 2, 1" {
+		t.Errorf("listAsString() expected '4, 3, 2, 1', found '" + listAsString + "'")
 	}
 }
